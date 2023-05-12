c8ebaecb022b9a39f9712d1473db0967df04d679
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-05-12 16:58:01 +0530

    Add files via upload
    Merging of multiple list in Java collection framework

---
+++ CF.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CF {
+    public static void main(String args []){
+        ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+        ArrayList<Integer> h = new ArrayList<>();
+        ArrayList<Integer> k = new ArrayList<>();
+        k.add(3);
+        k.add(6);
+        h.add(2);
+        h.add(4);
+        al.add(k);
+        al.add(h);
+        System.out.println(al);
+
+    }
+}
